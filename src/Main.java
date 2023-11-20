6bc73eda33920df0326e0358d9d4b58b9aeaf27a
Martin Rodriguez Passerini <118628544+rdgzpasserini@users.noreply.github.com>
2023-11-20 19:09:33 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 import java.util.Scanner;
-/** PAC DESARROLLO M03B 1S2324
- *  Sigue las especificaciones del enunciado de la pac de Desarrollo
- *  No se puede importar ninguna otra clase, dentro de esta clase.
- *  Obligatorio utilizar esta plantilla
- *  
- */
+
 public class Main {
 
     public static void main(String[] args) {
