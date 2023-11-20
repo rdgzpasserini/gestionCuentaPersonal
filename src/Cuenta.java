02343d725d1ae3640ef7770271b6adb64675ba49
Martin Rodriguez Passerini <118628544+rdgzpasserini@users.noreply.github.com>
2023-11-20 19:07:35 +0100

    Update Cuenta.java

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,12 +1,7 @@
 
 import java.util.ArrayList;
 import java.util.List;
-/** PAC DESARROLLO M03B 1S2324
- *  Sigue las especificaciones del enunciado de la pac de Desarrollo
- *  No se puede importar ninguna otra clase, dentro de esta clase.
- *  Obligatorio utilizar esta plantilla
- *  
- */
+
 public class Cuenta {
 
 	//Atributos
