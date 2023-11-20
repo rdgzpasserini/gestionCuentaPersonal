e440388be30a054e8e9e32f56ed99f4554690c27
Martin Rodriguez Passerini <118628544+rdgzpasserini@users.noreply.github.com>
2023-11-20 19:08:38 +0100

    Update Gasto.java

--- src/Gasto.java
+++ src/Gasto.java
@@ -1,9 +1,4 @@
-/** PAC DESARROLLO M03B 1S2324
- *  Sigue las especificaciones del enunciado de la pac de Desarrollo
- *  No se puede importar ninguna clase, dentro de esta clase.
- *  Obligatorio utilizar esta plantilla
- *  
- */
+
 public class Gasto extends Dinero {
 	//Constructor 
 	
